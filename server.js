7cc84debd42b01098c61de6ac888075956d3d18a
yoshidaranimahato-collab <yoshidaranimahato@gmail.com>
2026-08-29 15:13:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -238,7 +238,7 @@ function authenticate(
 ========================= */
 
 app.post(
-  "/api/register",
+  "/api/auth/register",
   async (req, res) => {
 
     try {
@@ -390,7 +390,7 @@ app.post(
 ========================= */
 
 app.post(
-  "/api/login",
+  "/api/auth/login",
   async (req, res) => {
 
     try {
